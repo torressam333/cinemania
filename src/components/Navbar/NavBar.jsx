04f07c9463d9f2c997534c4fbbc0dc03d6771216
Samuel <torressam333@gmail.com>
2023-10-19 10:38:07 -0700

    Style the navbar links and emojis

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,11 +1,32 @@
 import './NavBar.css';
+import Fire from '../../assets/emojis/fire.png';
+import GlowingStar from '../../assets/emojis/glowing-star.png';
+import Celebrate from '../../assets/emojis/partying-face.png';
 
 const NavBar = () => {
   return (
     <nav className='navbar'>
       <h1>Cinemania</h1>
       <div className='navbar_links'>
-        <a href='' className='navbar_link'>Popular</a>
+        <a>
+          Popular <img src={Fire} alt='fire emoji' className='navbar_emoji' />
+        </a>
+        <a>
+          Top Rated{' '}
+          <img
+            src={GlowingStar}
+            alt='glowing star emoji'
+            className='navbar_emoji'
+          />
+        </a>
+        <a>
+          Upcoming{' '}
+          <img
+            src={Celebrate}
+            alt='party face emoji'
+            className='navbar_emoji'
+          />
+        </a>
       </div>
     </nav>
   );
