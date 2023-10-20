12427e7ddbc740ac80cbd5613ab7a9238575a4a0
Samuel Torres <33882985+torressam333@users.noreply.github.com>
2023-10-20 16:23:59 -0700

    Add some filtering based on movie rating and add images for movies

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -1,28 +1,31 @@
-import './MovieCard.css';
-import StarEmoji from '../../assets/emojis/star.png';
+import "./MovieCard.css";
+import StarEmoji from "../../assets/emojis/star.png";
 
 const MovieCard = ({ movie }) => {
-  const {
-    title,
-    poster_path,
-    release_date,
-    vote_average,
-    vote_count,
-    overview,
-  } = movie;
+  const { id, title, poster_path, release_date, vote_average, overview } =
+    movie;
+
   return (
-    <a href='' className='movie_card'>
-      <img src={movie.poster_path} alt={title} className='movie_poster' />
-      <div className='movie_details'>
-        <h3 className='movie_details_heading'>{title}</h3>
-        <div className='align_flex_center movie_date_rate'>
+    <a
+      href={`https://www.themoviedb.org/movie/${id}`}
+      className="movie_card"
+      target="_blank"
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        alt={title}
+        className="movie_poster"
+      />
+      <div className="movie_details">
+        <h3 className="movie_details_heading">{title}</h3>
+        <div className="align_flex_center movie_date_rate">
           <p>{release_date}</p>
           <p>
-            {vote_average}{' '}
-            <img src={StarEmoji} alt='Star Emoji' className='card_emoji' />
+            {vote_average}{" "}
+            <img src={StarEmoji} alt="Star Emoji" className="card_emoji" />
           </p>
         </div>
-        <p className='movie_description'>{overview.slice(0, 100) + '...'}</p>
+        <p className="movie_description">{overview.slice(0, 100) + "..."}</p>
       </div>
     </a>
   );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,15 @@
-import './MovieList.css';
-import FireEmoji from '../../assets/emojis/fire.png';
-import MovieCard from './MovieCard';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import "./MovieList.css";
+import FireEmoji from "../../assets/emojis/fire.png";
+import MovieCard from "./MovieCard";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const MovieList = () => {
   // env vars in vite need `VITE` prefix
   const apiKey = import.meta.env.VITE_APP_TMBD_KEY;
   const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
   const [movies, setMovies] = useState([]);
+  const [minRating, setMinRating] = useState(0);
 
   const fetchMovies = async () => {
     try {
@@ -22,38 +23,56 @@ const MovieList = () => {
     }
   };
 
+  const handleFilter = (rating) => {
+    setMinRating(rating);
+
+    const filteredMovies = movies.filter(
+      (movie) => movie.vote_average >= minRating
+    );
+
+    setMovies(filteredMovies);
+  };
+
   useEffect(() => {
     fetchMovies();
   }, []);
 
   return (
-    <section className='movie_list'>
-      <header className='align_flex_center movie_list_header'>
-        <h2 className='align_flex_center movie_list_heading'>
-          Popular{' '}
-          <img src={FireEmoji} alt='Fire emoji' className='navbar_emoji' />
+    <section className="movie_list">
+      <header className="align_flex_center movie_list_header">
+        <h2 className="align_flex_center movie_list_heading">
+          Popular{" "}
+          <img src={FireEmoji} alt="Fire emoji" className="navbar_emoji" />
         </h2>
-        <div className='align_flex_center movie_list_fs'>
-          <ul className='align_flex_center movie_filter'>
-            <li className='movie_filter_item active'>7+ Star</li>
-            <li className='movie_filter_item'>8+ Star</li>
-            <li className='movie_filter_item'>9+ Star</li>
+        <div className="align_flex_center movie_list_fs">
+          <ul className="align_flex_center movie_filter">
+            <li
+              className="movie_filter_item active"
+              onClick={() => handleFilter(6)}
+            >
+              6+ Star
+            </li>
+            <li className="movie_filter_item" onClick={() => handleFilter(7)}>
+              7+ Star
+            </li>
+            <li className="movie_filter_item" onClick={() => handleFilter(8)}>
+              8+ Star
+            </li>
           </ul>
-          <select name='' id='' className='movie_sorting'>
-            <option value=''>Sort By</option>
-            <option value=''>Release Date</option>
-            <option value=''>Rating</option>
+          <select name="" id="" className="movie_sorting">
+            <option value="">Sort By</option>
+            <option value="">Release Date</option>
+            <option value="">Rating</option>
           </select>
-          <select name='' id='' className='movie_sorting'>
-            <option value=''>Ascending</option>
-            <option value=''>Descending</option>
+          <select name="" id="" className="movie_sorting">
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
           </select>
         </div>
       </header>
-      <div className='movie_cards'>
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+      <div className="movie_cards">
+        {movies &&
+          movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
       </div>
     </section>
   );
