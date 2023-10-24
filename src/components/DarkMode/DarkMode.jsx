50e2913b4327bb91021aa703a44166bc5157d97c
Samuel <torressam333@gmail.com>
2023-10-24 16:01:21 -0700

    Add more dark mode toggle logic

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 import './DarkMode.css';
 import Sun from '../../assets/Sun.svg?react';
 import Moon from '../../assets/Moon.svg?react';
 
 const DarkMode = () => {
+  const selectedTheme = () => localStorage.getItem('theme');
+  const [theme, setTheme] = useState(selectedTheme || 'light');
+  const [isDarkMode, setIsDarkMode] = useState(theme === 'dark');
+
+  useEffect(() => {
+    document.querySelector('body').setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+    setIsDarkMode(theme === 'dark');
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(isDarkMode ? 'light' : 'dark');
+  };
+
   return (
     <div className='dark_mode'>
-      <input className='dark_mode_input' type='checkbox' id='darkmode-toggle' />
+      <input
+        className='dark_mode_input'
+        type='checkbox'
+        id='darkmode-toggle'
+        onChange={toggleTheme}
+        checked={isDarkMode}
+      />
       <label className='dark_mode_label' htmlFor='darkmode-toggle'>
         <Sun />
         <Moon />
